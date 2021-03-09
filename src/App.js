d987a04459a4b413353dc67d7a5dbb02a4af35c9
Mike B <mikeberrio10@hotmail.com>
2021-03-09 18:05:03 -0500

    react search component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import {
+  BrowserRouter as router,
+  Route,
+  Router,
+  Switch,
+} from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     </div>
   );
 }
